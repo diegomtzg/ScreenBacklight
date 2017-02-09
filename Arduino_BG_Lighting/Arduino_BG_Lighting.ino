aaab80e99dc11c16362c87f28582eff65955132c
Diego Martinez <diegomtzg96@gmail.com>
2017-02-08 20:15:31 -0500

    project fully functional

--- Arduino_BG_Lighting/Arduino_BG_Lighting.ino
+++ Arduino_BG_Lighting/Arduino_BG_Lighting.ino
@@ -1,7 +1,8 @@
-int red, green, blue; //red, green and blue values
-int RedPin = 9; //Red pin 9 has a PWM
-int GreenPin = 10; //Green pin 10 has a PWM
-int BluePin = 11; //Blue pin 11 has a PWM
+#define BLUEPIN 9
+#define REDPIN 10
+#define GREENPIN 11
+
+int red, green, blue;
 
 void setup()
 {
@@ -11,25 +12,20 @@ void setup()
   int blue = 255;
   int green = 255;
 }
-
 void loop()
 {
   //protocol expects data in format of 4 bytes
   //(xff) as a marker to ensure proper synchronization always
   //followed by red, green, blue bytes
-  if (Serial.available()>=4) 
-  {
-    if(Serial.read() == 0xff)
-    {
+  if (Serial.available()>=4) {
+    if(Serial.read() == 0xff){
       red = Serial.read();
       green= Serial.read();
       blue = Serial.read();
     }
   }
-  
   //finally control led brightness through pulse-width modulation
-  analogWrite (RedPin, red);
-  analogWrite (GreenPin, green);
-  analogWrite (BluePin, blue);
-  delay(10);
+  analogWrite (REDPIN, red);
+  analogWrite (GREENPIN, green);
+  analogWrite (BLUEPIN, blue);
 }
